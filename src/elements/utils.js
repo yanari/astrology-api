1848148872858d9d23be01b0a3fd5e7c8c3cc5cb
Marcelle Yanari <yanarimy@gmail.com>
2023-05-05 21:12:24 -0300

    ?

--- src/elements/utils.js
+++ src/elements/utils.js
@@ -14,7 +14,7 @@ const reduceInto = (dispositorPoints, astroPoints) => {
   const all = [...Object.entries(dispositorPoints), ...Object.entries(astroPoints),];
   const points = all.reduce((accumulator, [key, value], i) => {
     let pointToAdd = 0;
-    const foundIndex = accumulator.findIndex((item) => key === item.name)
+    const foundIndex = accumulator.findIndex((item) => key === item?.name)
     const index = foundIndex > -1 ? foundIndex : i;
 
     if (accumulator[index]) {
